65324d21bf927ef831edcdad4eb0f35d4f4e2cfd
SanjaiPG <sanjaigukesan8686@gmail.com>
2026-02-03 08:32:26 +0530

    spring boot setup with postgresql connected

---
+++ securevote/backend/src/main/java/com/securevote/backend/SecurevoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.securevote.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurevoteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurevoteBackendApplication.class, args);
+	}
+
+}
---
+++ securevote/backend/src/test/java/com/securevote/backend/SecurevoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securevote.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurevoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
